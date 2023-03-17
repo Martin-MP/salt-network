d1dd40906ef848bd48ca2c1293070ece2a35680d
MartinAPF <perez.martin@apfutura.net>
2023-03-17 10:27:38 +0100

    Update autohosting.py

--- scripts/autohosting.py
+++ scripts/autohosting.py
@@ -3,34 +3,38 @@ import os
 
 class User:
 
-    def __init__(self, user, domains):
-        self.user = user
+    def __init__(self, name, domains):
+        self.name = name
         self.domains = domains
     
 
 def get_domains():
     users = list()
-    for user in os.listdir('/var/www'):
+    for name in os.listdir('/var/www'):
         domains = dict()
-        for domain in os.listdir(f'/var/www/{user}/'):
-            domains[domain] = f'/var/www/{user}/{domain}'
-        users.append(User(user, domains))
+        for domain in os.listdir(f'/var/www/{name}/'):
+            domains[domain] = f'/var/www/{name}/{domain}'
+        users.append(User(name, domains))
     return users
 
 
 def print_domains(users):
     for user in users:
-        if user == user[0]:
-            if user == user[-1]:
-                print(f"    ━━{user.user}")
+        if user.name == user[0].name:
+            if user.name == user[-1].name:
+                print(f"    ━━{user.name}")
             else:
-                print(f"    ┏━{user.user}")
-        elif user == user[-1]:
-            print(f"    ┗━{user.user}")
+                print(f"    ┏━{user.name}")
+        elif user.name == user[-1].name:
+            print(f"    ┗━{user.name}")
         else:
-            print(f"    ┣━{user.user}")
+            print(f"    ┣━{user.name}")
         for domain in user.domains:
             if domain == user.domains[-1]:
                 print(f"        ┗━{domain}")
             else:
                 print(f"        ┣━{domain}")
+
+
+users = get_domains()
+print_domains(users)
