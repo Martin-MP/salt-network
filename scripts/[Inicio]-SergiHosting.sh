a5c11de48c7d6502a039c2f595c06dcf94fc6314
MartinAPF <perez.martin@apfutura.net>
2023-03-21 11:47:23 +0100

    Update [Inicio]-SergiHosting.sh

--- scripts/[Inicio]-SergiHosting.sh
+++ scripts/[Inicio]-SergiHosting.sh
@@ -1,62 +1,37 @@
 #!/bin/bash
-
 clear
-
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 BLUE='\033[0;34m'
- 
 while [ x$username = "x" ]; do
-
 read -p "Pon el nombre de usuario sin espacios : " username
-
 if id -u $username >/dev/null 2>&1; then
 echo "Usuario existente"
 username=""
-
 fi
-
 done
 printf "\n${BLUE}>> Usuario Creado [$username] ${NC}\n"
 while [ x$group = "x" ]; do
 read -p "Pon grupo primario, si no existe se creará : " group
-
 if id -g $group >/dev/null 2>&1; then
-
 echo "Grupo existente, si quieres usar uno existente cambialo manualmente más tarde"
-
 else
-
 groupadd $group
-
 fi
-
 done
 printf "\n${BLUE}>> $group creado para [$username] ${NC}\n"
 read -p "Pon el bash [/bin/bash] : " bash
-
 if [ x"$bash" = "x" ]; then
-
 bash="/bin/bash"
 fi
-
 read -p "Pon el homedir [/home/$username] : " homedir
-
 if [ x"$homedir" = "x" ]; then
-
 homedir="/home/$username"
-
 fi
-
-
 read -p "Confirma [grupo:$group][usuario:$username] [y/n]" confirm
-
 if [ "$confirm" = "y" ]; then
-
 useradd -g $group -s $bash -d $homedir -m $username
-
 fi
-
 passwd $username
 printf "\n${BLUE}>> [$username] satisfactoramiente creado con [$group] ${NC}\n"
 chown $username:$group $homedir
@@ -67,55 +42,54 @@ then
 printf "\n\t\t${BLUE}[CREANDO VIRTUAL HOST]${NC}\n\n"
  
 printf "\n${BLUE}>> Creando carpetas para [$domain] ${NC}\n"
-mkdir -p /var/www/$domain.nosekeponer.local/public_html/
+mkdir -p /var/www/$domain/public_html/
 printf "\n${BLUE}>> Estableciendo permisos para usuario [$username] en sitio [$domain] ${NC}\n"
 
-chmod 755 /var/www/$domain.nosekeponer.local/
-chmod 755 /var/www/$domain.nosekeponer.local/public_html/
-chown root:root /var/www/$domain.nosekeponer.local
-chown $username:$group /var/www/$domain.nosekeponer.local/public_html/
-touch /var/www/$domain.nosekeponer.local/public_html/index.html
-chown $username:$group /var/www/$domain.nosekeponer.local/public_html/index.html
-chmod 755 /var/www/$domain.nosekeponer.local/public_html/index.html
+chmod 755 /var/www/$domain/
+chmod 755 /var/www/$domain/public_html/
+chown root:root /var/www/$domain
+chown $username:$group /var/www/$domain/public_html/
+touch /var/www/$domain/public_html/index.html
+chown $username:$group /var/www/$domain/public_html/index.html
+chmod 755 /var/www/$domain/public_html/index.html
 printf "\n${BLUE}>> Creando archivos de configuracion para sitio [$domain] ${NC}\n"
-touch /etc/apache2/sites-available/$domain.nosekeponer.local.conf
+touch /etc/apache2/sites-available/$domain.conf
 
 echo "<VirtualHost *:80>
-  ServerName $domain.nosekeponer.local
-  Redirect / https://$domain.nosekeponer.local
+  ServerName $domain
+  Redirect / https://$domain
 </VirtualHost>
 <VirtualHost *:443>
   ServerAdmin admin@$domain
-  ServerName $domain.nosekeponer.local
-  ServerAlias www.$domain.nosekeponer.local
-  DocumentRoot /var/www/$domain.nosekeponer.local/public_html
+  ServerName $domain
+  ServerAlias www.$domain
+  DocumentRoot /var/www/$domain/public_html
   ErrorLog ${APACHE_LOG_DIR}/error.log
   CustomLog ${APACHE_LOG_DIR}/access.log combined
   SSLEngine on
   SSLCertificateFile /etc/apache2/certificate/apache-certificate.crt
   SSLCertificateKeyFile /etc/apache2/certificate/apache.key
-</VirtualHost>" > /etc/apache2/sites-available/$domain.nosekeponer.local.conf
+</VirtualHost>" > /etc/apache2/sites-available/$domain.conf
  echo "Match Group $group
-  ChrootDirectory /var/www/$domain.nosekeponer.local/
+  ChrootDirectory /var/www/$domain/
   AllowTCPForwarding no
   X11Forwarding no
   ForceCommand internal-sftp
   " >> /etc/ssh/sshd_config
-systemctl restart ssh  
+systemctl restart ssh
 printf "\n${BLUE}>> Habilitando sitio [$domain] ${NC}\n\n"
-a2ensite $domain.nosekeponer.local.conf
+a2ensite $domain.conf
 echo "<!DOCTYPE html>
 <html>
 <body>
 <h1>La teva pagina $username</h1>
-<h2>puja el teu $domain.nosekeponer.local</h2>
+<h2>puja el teu $domain</h2>
 </body>
-</html>" >> /var/www/$domain.nosekeponer.local/public_html/index.html
+</html>" >> /var/www/$domain/public_html/index.html
 
 printf "\n${BLUE}>> Reiniciando apache2 ${NC}\n"
 systemctl reload apache2
-printf "\n${BLUE}>> Añadiendo sitio [$domain.nosekeponer.local] a host[local]${NC}\n"
-echo "192.168.7.14  $domain.nosekeponer.local" >> /etc/hosts
-printf "\n${BLUE}>> VIRTUALHOST PARA SITIO [http://$domain.nosekeponer.local] HABILITADO${NC}\n"
-
+printf "\n${BLUE}>> Añadiendo sitio [$domain] a host[local]${NC}\n"
+echo "192.168.7.14  $domain" >> /etc/hosts
+printf "\n${BLUE}>> VIRTUALHOST PARA SITIO [http://$domain] HABILITADO${NC}\n"
 fi
