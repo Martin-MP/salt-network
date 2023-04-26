76c2d936244eb996f03246967f3e2ad497df61a3
Martin-MP <martin.perez.fontana@alumnat.ins-mediterrania.cat>
2023-04-26 19:01:55 +0200

    log

--- scripts/one_punch.py
+++ scripts/one_punch.py
@@ -7,22 +7,35 @@ class Minion:
         self.ip = ip
         self.id = minion_id
         self.up = False
+        self.calculate_log_name()
+        
+    def calculate_log_name(self):
+        base_path = f"/root/salt_logs/{time.strftime('%Y%m%d')}-{self.name}.log"
+        if os.path.isfile(base_path):
+            i = 1
+            while os.path.isfile(f"{base_path}_{i}"):
+                i += 1
+            self.log_name = f"{self.name}_{i}.log"
+        else:
+            self.log_name = f"{self.name}.log"
 
     def set_up(self):
         self.up = True
 
     def check_up(self):
-        response = os.system(f"ping -c 1 {self.ip} > /dev/null 2>&1")
-        if response == 0:
+        pid_code = os.system(f"ping -c 1 {self.ip} > /dev/null 2>&1")
+        if pid_code == 0:
             self.set_up()
             return True
         else:
             return False
-    
+
     def apply_state(self):
-        #response = os.popen(f"salt '*{self.id}*' state.apply").read()
-        response = os.system(f"salt '*{self.id}*' state.apply")
-        print(response)
+        pid_code = os.system(f"salt '*{self.id}*' state.apply > /root/salt_logs/{self.log_name} 2>&1")
+        if pid_code == 0:
+            print(f"Successfully applied state on {self.name}")
+        else:
+            print(f"Failed to apply state on {self.name}")
 
 
 def check_all_up(timeout=20):
